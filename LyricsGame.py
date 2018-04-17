058b328cf997cc9e40f4696c9af621ed54c6c891
vaiva <va29@st-andrews.ac.uk>
2018-04-17 18:02:50 +0100

    reads json lyrics

--- LyricsGame.py
+++ LyricsGame.py
@@ -1,12 +1,24 @@
 from flask import Flask, abort
+import json
 from flask_ask import Ask, delegate, statement
 
 app = Flask(__name__)
 ask = Ask(app, '/')
 
+
+def getLyrics(file):
+    data = json.load(open(file))
+    lyrics = data['message']['body']['lyrics']['lyrics_body'].split('\n')
+    return lyrics[:-3]
+
 @ask.intent('StartGame')
 def start_game():
-    return
+    file = "data/taylor-swift-lyrics.json"
+
+    lyrics = getLyrics(file)
+    print(lyrics)
+    return statement(lyrics[0]+'\n'+lyrics[1])
 
 if __name__ == '__main__':
     app.run(debug=True)
+
